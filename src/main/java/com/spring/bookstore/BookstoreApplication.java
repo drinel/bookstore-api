382751179f5dba6a70906d8b29b467ec48197419
drinel <will.i.am.002@hotmail.com>
2021-12-16 23:58:27 -0300

    Criação perfil 'dev'

--- src/main/java/com/spring/bookstore/BookstoreApplication.java
+++ src/main/java/com/spring/bookstore/BookstoreApplication.java
@@ -1,15 +1,7 @@
 package com.spring.bookstore;
 
-import com.spring.bookstore.domain.Categoria;
-import com.spring.bookstore.domain.Livro;
-import com.spring.bookstore.repositories.CategoriaRepository;
-import com.spring.bookstore.repositories.LivroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class BookstoreApplication {
